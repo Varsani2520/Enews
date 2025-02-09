8635ee5d8dac7065756223c02a3c0cac95d5f3bc
Varsani2520 <varsaniranjani@gmail.com>
2025-02-09 14:53:16 +0530

    in social media icon add bg color

--- app/Reuse/Icons.jsx
+++ app/Reuse/Icons.jsx
@@ -1,12 +1,24 @@
-import { IconButton } from '@mui/material'
-import React from 'react'
+import { IconButton } from "@mui/material";
+import React from "react";
 
 const Icons = ({ onClick, icon, props, href }) => {
-    return (
-        <IconButton onClick={onClick} {...props} color='inherit' href={href} target="_blank">
-            {icon}
-        </IconButton>
-    )
-}
+  return (
+    <IconButton
+      onClick={onClick}
+      {...props}
+      color="inherit"
+      href={href}
+      target="_blank"
+      sx={{
+        "&:hover": {
+          background: "#ce2b2b",
+          borderRadius: "5px"
+        },
+      }}
+    >
+      {icon}
+    </IconButton>
+  );
+};
 
-export default Icons
+export default Icons;

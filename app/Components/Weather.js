d316adfd4d5def4491adf4ec67da2e4c8e59247a
Varsani2520 <varsaniranjani@gmail.com>
2025-02-12 16:30:43 +0530

    add text shadow on date

--- app/Components/Weather.js
+++ app/Components/Weather.js
@@ -50,6 +50,7 @@ const Weather = () => {
             alignItems: "center",
             backgroundColor: "#f20404",
             color: "white",
+            textShadow: "1px 1px 2px rgba(0, 0, 0, 0.7)",
             fontWeight: "bold",
             borderRadius: "10px",
             fontSize: "small",

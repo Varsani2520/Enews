65cab1755d238d98e710940c77940aeccffe1873
Varsani2520 <varsaniranjani@gmail.com>
2025-02-24 18:05:26 +0530

    db addd

--- app/ClientLayout.js
+++ app/ClientLayout.js
@@ -4,20 +4,25 @@ import { useEffect } from "react";
 import Weather from "./Components/Weather";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
-import { registerServiceWorker, requestNotificationPermission } from "./utils/firebase";
+import {
+  auth,
+  registerServiceWorker,
+  requestNotificationPermission,
+} from "./utils/firebase";
 import Navigation from "./Components/Navigation";
-
+import { useAuthState } from "react-firebase-hooks/auth";
 export default function ClientLayout({ children }) {
+  const [user] = useAuthState(auth);
   useEffect(() => {
-    if (typeof window !== "undefined") {
-    const setupFCM = async () => {
-      await registerServiceWorker();
-      await requestNotificationPermission();
-    };
+    if (typeof window !== "undefined" && user?.uid) {
+      const setupFCM = async () => {
+        await registerServiceWorker();
+        await requestNotificationPermission(user.uid);
+      };
 
-    setupFCM();
-  }
-  }, []);
+      setupFCM();
+    }
+  }, [user]);
 
   return (
     <>

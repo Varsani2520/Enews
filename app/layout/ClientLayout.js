d760e549e3f452544b5c6f85f40a47b126781e66
Varsani2520 <varsaniranjani@gmail.com>
2025-04-06 21:23:41 +0530

    try to display ads in mobile device

--- app/layout/ClientLayout.js
+++ app/layout/ClientLayout.js
@@ -13,6 +13,7 @@ import Navigation from "../components/layout/Navigation";
 import Header from "../components/layout/Header";
 import Footer from "../components/layout/Footer";
 import { useThemeContext } from "../context/ThemeContext";
+import AdBanner from "../components/features/AdBanner";
 export default function ClientLayout({ children }) {
   const [user] = useAuthState(auth);
 const {themeData}=useThemeContext()
@@ -35,6 +36,7 @@ const {themeData}=useThemeContext()
       <Weather />
       <Navigation />
       <Header />
+      <AdBanner/>
       {children}
       <Footer />
     </div>

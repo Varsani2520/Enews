16395d9ffeb32487c12595a750ad4b9ed8401251
Varsani2520 <varsaniranjani@gmail.com>
2025-04-07 16:29:13 +0530

    add ads in detail page

--- app/components/features/GoogleAd.jsx
+++ app/components/features/GoogleAd.jsx
@@ -13,17 +13,22 @@ const GoogleAd = () => {
   }, []);
 
   return (
-    <>
-      {/* Google AdSense Script (added only once globally, ideally in _document.tsx or layout) */}
-      <ins
-        className="adsbygoogle"
-        style={{ display: "block", textAlign: "center" }}
-        data-ad-layout="in-article"
-        data-ad-format="fluid"
-        data-ad-client="ca-pub-6580779703282784"
-        data-ad-slot="6729903768"
-      />
-    </>
+    <div className="w-full flex justify-center py-4">
+      <div className="bg-slate-900 rounded-lg p-4 w-full max-w-[728px]">
+        <ins
+          className="adsbygoogle"
+          style={{
+            display: "block",
+            textAlign: "center",
+            width: "100%",
+          }}
+          data-ad-layout="in-article"
+          data-ad-format="fluid"
+          data-ad-client="ca-pub-6580779703282784"
+          data-ad-slot="6729903768"
+        />
+      </div>
+    </div>
   );
 };
 

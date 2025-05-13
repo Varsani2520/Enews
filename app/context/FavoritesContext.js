4564787e624b3a95f2359a5a8fbe31654afe779c
Soni Dhruv <dhruvrsoni04@gmail.com>
2025-05-13 11:28:58 +0530

    fav service and cutom hook

---
+++ app/context/FavoritesContext.js
@@ -0,0 +1,38 @@
+import { createContext, useContext, useState, useEffect } from "react";
+import { getFavorites } from "../service/favs"; // Import your API calls
+
+const FavoritesContext = createContext();
+
+export const useFavorites = () => {
+  return useContext(FavoritesContext);
+};
+
+export const FavoritesProvider = ({ children }) => {
+  const [favorites, setFavorites] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchFavorites = async () => {
+      try {
+        setLoading(true);
+        const res = await getFavorites();
+        setFavorites(res.data.favorites.articles || []);
+      } catch (err) {
+        setError(err.message || "Failed to fetch favorites");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchFavorites();
+  }, []);
+
+  return (
+    <FavoritesContext.Provider
+      value={{ favorites, loading, error, setFavorites }}
+    >
+      {children}
+    </FavoritesContext.Provider>
+  );
+};

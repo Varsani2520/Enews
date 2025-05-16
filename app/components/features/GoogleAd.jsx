f3d374abcc725e0b8858df969a7570f2a7e569bd
Varsani2520 <varsaniranjani@gmail.com>
2025-05-16 14:31:58 +0530

    ads

--- app/components/features/GoogleAd.jsx
+++ app/components/features/GoogleAd.jsx
@@ -17,10 +17,8 @@ const GoogleAd = () => {
       className="adsbygoogle"
       style={{
         display: "block",
-        width: "100%",         // full width of parent container
-        maxWidth: "1200px",    // max width for large screens
+        width: "100%",     // fill the container
         height: "90px",
-        margin: "0 auto",      // center horizontally if smaller than maxWidth
       }}
       data-ad-client="ca-pub-6580779703282784"
       data-ad-slot="1338579894"
--- app/components/shared/Banner.jsx
+++ app/components/shared/Banner.jsx
@@ -1,32 +1,14 @@
 "use client";
 import React from "react";
-import { Container } from "@mui/material";
 import GoogleAd from "../features/GoogleAd";
 
 const Banner = () => {
   return (
-    <Container
-      maxWidth="xl"
-      className="mt-4"
-      sx={{
-        display: { xs: "none", sm: "none", md: "block" },
-        px: 0,                 // Remove horizontal padding to avoid shrinking container
-      }}
-    >
-      <div
-        style={{
-          width: "100%",
-          maxWidth: "1200px",
-          marginLeft: "auto",
-          marginRight: "auto",
-          overflow: "hidden",   // Hide overflow if any
-          borderRadius: "8px",
-          boxShadow: "0 4px 8px rgba(0,0,0,0.1)",
-        }}
-      >
+    <div className="mt-4 max-w-screen-lg mx-auto px-4">
+      <div className="overflow-hidden rounded-lg shadow-lg">
         <GoogleAd />
       </div>
-    </Container>
+    </div>
   );
 };
 

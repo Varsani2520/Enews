5b55279428dfadf0537cc6beee9f9d9b705886ad
Varsani2520 <varsaniranjani@gmail.com>
2025-05-16 17:08:54 +0530

    try to fit ads in banner

--- app/components/features/GoogleAd.jsx
+++ app/components/features/GoogleAd.jsx
@@ -17,7 +17,7 @@ const GoogleAd = () => {
     <ins
       className="adsbygoogle"
       style={{
-        display: "block", width: "100%"
+        display: "block", width: "100%",height:"90px"
       }}
       data-ad-client="ca-pub-6580779703282784"
       data-ad-slot="1338579894"

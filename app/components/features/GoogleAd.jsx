366b2df0a947d11ea9fea1b8b16d7d5ce12fc85b
Varsani2520 <varsaniranjani@gmail.com>
2025-05-16 16:36:41 +0530

    banner ads try to display

--- app/components/features/GoogleAd.jsx
+++ app/components/features/GoogleAd.jsx
@@ -2,27 +2,25 @@
 import { useEffect } from "react";
 
 const GoogleAd = () => {
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      try {
-        (window.adsbygoogle = window.adsbygoogle || []).push({});
-      } catch (e) {
-        console.error("Adsbygoogle error:", e);
-      }
-    }
-  }, []);
+ useEffect(() => {
+  console.log("GoogleAd mounted");
+  try {
+    (window.adsbygoogle = window.adsbygoogle || []).push({});
+    console.log("Ad pushed");
+  } catch (e) {
+    console.error("Adsbygoogle error:", e);
+  }
+}, []);
+
 
   return (
     <ins
       className="adsbygoogle"
-      style={{
-        display: "block",
-        width: "100%",  // Make it responsive inside container
-        height: "90px",
-      }}
+      style={{ display: "inline-block", width: "1200px", height: "90px" }}
       data-ad-client="ca-pub-6580779703282784"
       data-ad-slot="1338579894"
     />
+
   );
 };
 
